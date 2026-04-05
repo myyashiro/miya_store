ebab5b45d8d79681104841cac0eb9e0cccfc6f1c
Marco Yashiro <myashiro@quantizbr.onmicrosoft.com>
2026-04-05 08:06:32 -0300

    Fix revalidate route: add force-dynamic

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,6 +1,8 @@
 import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get('secret');
 
